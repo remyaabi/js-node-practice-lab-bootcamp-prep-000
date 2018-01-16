9623a3faa5a38c919d02a1280264e6c4ca2e3472
Remya Jacob <remyaabi@gmail.com>
2018-01-16 00:10:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ console.log("Name:")
 console.log(name)
 
 console.log("Height:")
+<<<<<<< HEAD
 console.log(height + 5)
+=======
+console.log(height +5)
+>>>>>>> a0b80118ece88bb83626d0e0c1c63910a50c1788
